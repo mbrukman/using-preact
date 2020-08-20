adde73ced94c59ba9bf6abdb82949369f60ef214
Misha Brukman <mbrukman@google.com>
2020-08-20 12:24:12 -0400

    Add support for Bazel and Closure Compiler

--- third_party/preact-without-babel/README.md
+++ third_party/preact-without-babel/README.md
@@ -8,6 +8,28 @@ Examples are included for both "Classful" Components, and stateless pure functio
 
 > [View Demo on JSFiddle](http://jsfiddle.net/developit/e281k4wz/)
 
+## Building with Bazel + Closure Compiler
+
+Opening the uncompiled version: just load [`index.html`](index.html) in your
+browser, which will interpret [`index.js`](index.js), which works as is.
+
+To compile with Bazel and Closure Compiler, which will typecheck the file and
+minify it in one step:
+
+```
+$ bazel build //third_party/preact-without-babel:{app,app_html}
+```
+
+This command actually builds two targets:
+
+* `:app` which builds the minified JavaScript "binary", which compiles
+  `index.js` to `app.js`
+* `:app_html` which creates a new file` app.html` from `index.html` to update
+  the JavaScript input from the uncompiled `index.js` to the compiled `app.js`
+
+Then, open `blaze-bin/third_party/preact-wihout-babel/app.html` in your browser,
+and verify that it works just the same as the uncompiled version above.
+
 ---
 
 > :horse: Supporting older browsers?  Check out [Preact in ES3](https://github.com/developit/preact-in-es3)
--- third_party/preact-without-babel/index.js
+++ third_party/preact-without-babel/index.js
@@ -1,16 +1,28 @@
-'use strict';
-
-const { Component, h, render } = window.preact;
+const { Component, h, render, VNode } = window.preact;
 
 
-/** Example classful component */
+/**
+ * Example classful component.
+ *
+ * @extends {Component<!Object<string, *>, {message: string}>}
+*/
 class App extends Component {
+  /**
+   * @override
+   */
   componentDidMount() {
-    this.setState({ message:'Hello!' });
+    this.setState({ message: 'Hello!' });
   }
+
+  /**
+   * @param {!Object=} props
+   * @param {{message: ?string}=} state
+   * @return {!VNode<*>}
+   * @override
+   */
   render(props, state) {
     return (
-      h('div', {id:'app'},
+      h('div', { id: 'app' },
         h(Header, { message: state.message }),
         h(Main)
       )
@@ -19,20 +31,30 @@ class App extends Component {
 }
 
 
-/** Components can just be pure functions */
-const Header = (props) => {
+/**
+ * Components can just be pure functions.
+ *
+ * @param {{message: ?string}} props
+ * @return {!VNode<*>}
+ */
+function Header(props) {
   return h('header', null,
     h('h1', null, 'App'),
     props.message && h('h2', null, props.message)
   );
-};
+}
 
 
 /** Instead of JSX, use: h(type, props, ...children) */
 class Main extends Component {
+  /**
+   * @override
+   * @return {!VNode<*>}
+   */
   render() {
-    const items = [1,2,3,4,5].map( (item) => (
-      h('li', {id:item}, 'Item '+item)
+    /** @type {!Array<!VNode<*>>} */
+    const items = [1, 2, 3, 4, 5].map((/** @type {number} */ item) => (
+      h('li', { id: item }, 'Item ' + item)
     ));
     return (
       h('main', null,
